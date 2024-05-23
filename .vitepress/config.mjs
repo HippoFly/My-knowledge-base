17aec363de3b6dfae1fdec73b4fccdbf746d61aa
FlyHippo <whoarewe4u@protonmail.com>
2024-05-23 20:11:15 +0800

    fix: 没有修改base导致部署后css找不到的问题

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "MyVitePressProject",
+  base: '/A-Vitepress-Demo/',
   description: "A VitePress Site",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
